73ebd930c0c30de0d6821a8244f5165246a5e2ec
paulorbsfilho <p4ulinho@outlook.com>
2018-09-27 10:52:52 -0300

    Inicio.

--- core.py
+++ core.py
@@ -1,4 +1,3 @@
-from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import requests
 import re
@@ -6,24 +5,16 @@ import re
 def main():
     keyword = "google"#input("Digite sua busca:\n")
     url = "https://www.google.com/" #input("Digite a url de inicio:\n")
-    depth = "1"#input("digite a profundidade\n")
+    depth = "3"#input("digite a profundidade\n")
     search(keyword, url, depth)
 
 def search(keyword, url, depth):
-    print()
-    a = url[-6]
     response = requests.get(url)
-    reg = re.compile(r'href="(.*?)"')
-    links = []
-    for l in response.text.split():
-        links += reg.findall(l)
-    print(links)
-# html = urlopen("https://www.google.com/")
-# res = BeautifulSoup(html.read(), "html5lib");
-# print(res.title)
-#
-# response = requests.get('http://www.google.com/')
-# print(response.status_code)
+    print(response.status_code)
+    html = BeautifulSoup(response.text, 'html5lib')
+    links = html.find_all("a")
+    for link in links:
+        print(link['href'])
 
 if __name__ == '__main__':
     main()
