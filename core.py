085e19b19df267e050cd9d44c8aec658e53277f1
paulorbsfilho <p4ulinho@outlook.com>
2018-09-27 12:02:26 -0300

    Correção bug dos links

--- core.py
+++ core.py
@@ -1,20 +1,31 @@
 from bs4 import BeautifulSoup
-import requests
-import re
+import requests, re, time
 
 def main():
-    keyword = "google"#input("Digite sua busca:\n")
-    url = "https://www.google.com/" #input("Digite a url de inicio:\n")
+    keyword = "alunos"#input("Digite sua busca:\n")
+    url = "http://libra.ifpi.edu.br/" #input("Digite a url de inicio:\n")
     depth = "3"#input("digite a profundidade\n")
     search(keyword, url, depth)
 
 def search(keyword, url, depth):
+    start = time.time()
     response = requests.get(url)
-    print(response.status_code)
     html = BeautifulSoup(response.text, 'html5lib')
     links = html.find_all("a")
     for link in links:
-        print(link['href'])
+        try:
+            print(link['href'])
+        except KeyError:
+            print(link)
+    text = html.text
+    t = text.split()
+    try:
+        s = t.index(keyword)
+        print(s)
+    except ValueError:
+        print(u"\nPalavra ou expressão nao encontrada dentro da página.")
+    end = time.time()
+    print(u"Tempo de execução:", end - start)
 
 if __name__ == '__main__':
     main()
