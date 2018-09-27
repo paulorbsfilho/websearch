abad8ae97065b3d88e1b393499776dd3bf9f00be
paulorbsfilho <p4ulinho@outlook.com>
2018-09-27 14:13:29 -0300

    Busca da palavra.

--- core.py
+++ core.py
@@ -1,11 +1,13 @@
 from bs4 import BeautifulSoup
 import requests, re, time
 
+
 def main():
-    keyword = "Federal"#input("Digite sua busca:\n")
-    url = "http://libra.ifpi.edu.br/" #input("Digite a url de inicio:\n")
-    depth = 3#input("digite a profundidade\n")
+    keyword = "Federal"  # input("Digite sua busca:\n")
+    url = "http://libra.ifpi.edu.br/"  # input("Digite a url de inicio:\n")
+    depth = 3  # input("digite a profundidade\n")
     search(keyword, url, depth)
+
 
 def search(keyword, url, depth):
     start = time.time()
@@ -19,8 +21,6 @@ def search(keyword, url, depth):
             print(link)
     text = html.text
     t = text.split()
-    # print(type(t))
-    # print(t)
     try:
         s = t.index(keyword)
         print(s)
@@ -28,14 +28,14 @@ def search(keyword, url, depth):
         print(u"\nPalavra ou expressão nao encontrada dentro da página.")
     tam = len(t)
     v = []
-    cp = 5
+    cp = 2
 
-    while cp !=0:
-        v.append(t[s-cp])
+    while cp != 0:
+        v.append(t[s - cp])
         cp -= 1
 
     c = 0
-    while c < 5:
+    while c < 3:
         if s + c < tam:
             v.append(t[s + c])
             c += 1
@@ -44,8 +44,9 @@ def search(keyword, url, depth):
     end = time.time()
 
     res = " ".join(v)
-    print(print ("\n Palavra Buscada: " + keyword + res + "\nLink: " + str(url)))
+    print(u"\nSua busca retornou: " + keyword + ". "+ res + "\nLink: " + url)
     print(u"Tempo de execução:", end - start)
+
 
 if __name__ == '__main__':
     main()
