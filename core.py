52bf91e3bfe3ab59a516c681c3e903ba4c925661
paulorbsfilho <p4ulinho@outlook.com>
2018-10-01 12:47:38 -0300

    Quebrando as responsabilidades.

--- core.py
+++ core.py
@@ -1,11 +1,10 @@
 from bs4 import BeautifulSoup
 import requests, re, time
 
-
 def main():
-    keyword = u"Inscrição"  # input("Digite sua busca:\n")
-    url = "http://libra.ifpi.edu.br/"  # input("Digite a url de inicio:\n")
-    depth = 3  # input("digite a profundidade\n")
+    keyword = "Federal"#input("Digite sua busca:\n")
+    url = "http://www.globoesporte.com/" #input("Digite a url de inicio:\n")
+    depth = 3#input("digite a profundidade\n")
     search(keyword, url, depth)
 
 
@@ -14,39 +13,15 @@ def search(keyword, url, depth):
     response = requests.get(url)
     html = BeautifulSoup(response.text, 'html5lib')
     links = html.find_all("a")
-    for link in links:
-        try:
-            print(link['href'])
-        except KeyError:
-            print(link)
-    text = html.text
-    t = text.split()
-    s = 0
-    try:
-        s = t.index(keyword)
-        print(s)
-    except ValueError:
-        print(u"\nPalavra ou expressão nao encontrada dentro da página.")
-    tam = len(t)
-    v = []
-    cp = 2
-
-    while cp != 0:
-        v.append(t[s - cp])
-        cp -= 1
-
-    c = 0
-    while c < 3:
-        if s + c < tam:
-            v.append(t[s + c])
-            c += 1
-        else:
-            break
+    for l in links:
+        print(l.get('href'))
+    searchInPage(keyword,html)
     end = time.time()
-
-    res = " ".join(v)
-    print(u"\nSua busca retornou: " + keyword + ". "+ res + "\nLink: " + url)
     print(u"Tempo de execução:", end - start)
+
+
+def searchInPage(keyword,html):
+    pass
 
 
 if __name__ == '__main__':
