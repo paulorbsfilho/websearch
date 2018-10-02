d1b98f2ab9be931f407d1bb6fa840bd25d5c0bbf
paulorbsfilho <p4ulinho@outlook.com>
2018-10-01 22:21:23 -0300

    Lista das mais acessadas.

--- core.py
+++ core.py
@@ -1,10 +1,10 @@
 from bs4 import BeautifulSoup
-import requests, time
+import requests, re, time
 
 
 def main():
-    keyword = u"Inscrição"  # input("Digite sua busca:\n")
-    url = "http://libra.ifpi.edu.br/"  # input("Digite a url de inicio:\n")
+    keyword = u"O Sol"  # input("Digite sua busca:\n")
+    url = "http://cifraclub.com.br/"  # input("Digite a url de inicio:\n")
     depth = 3  # input("digite a profundidade\n")
     search(keyword, url, depth)
 
@@ -13,37 +13,25 @@ def search(keyword, url, depth):
     start = time.time()
     response = requests.get(url)
     html = BeautifulSoup(response.text, 'html5lib')
-    links = html.find_all("a")
-    for link in links:
-        try:
-            print(link['href'])
-        except KeyError:
-            print(u"Não é um link")
-    text = html.text
-    t = text.split()
-    s = 0
-    try:
-        s = t.index(keyword)
-        print(type(s))
-    except ValueError:
-        print(u"\nPalavra ou expressão nao encontrada dentro da página.")
-    tam = len(t)
-    v = []
-    cp = 2
-    while cp != 0:
-        v.append(t[s - cp])
-        cp -= 1
-    c = 0
-    while c < 3:
-        if s + c < tam:
-            v.append(t[s + c])
-            c += 1
+    nomes = html.find_all("strong", class_="top-txt_primary")
+    lista_mais_acessadas = []
+    casos = 0
+    res = ""
+    for n in nomes:
+        print(n.text)
+        lista_mais_acessadas += n
+    for i in lista_mais_acessadas:
+        if keyword in lista_mais_acessadas:
+            casos = casos + 1
         else:
-            break
+            print("false")
     end = time.time()
+    if casos < 1:
+        res = u"não está entre as mais tocadas."
+    else:
+        res = u"está entre as mais tocadas em:"
 
-    res = " ".join(v)
-    print(u"\nSua busca retornou: " + keyword + ". "+ res + "\nLink: " + url)
+    print(u"\nSua busca retornou: " + "\"" + keyword + "\" " + res + "\n" + url)
     print(u"Tempo de execução:", end - start)
 
 
