d8e09c2e311518716a68b0e3e2f412867511ee32
paulorbsfilho <p4ulinho@outlook.com>
2018-10-01 22:27:27 -0300

    Lista das mais acessadas.

--- core.py
+++ core.py
@@ -1,9 +1,9 @@
 from bs4 import BeautifulSoup
-import requests, re, time
+import requests, time
 
 
 def main():
-    keyword = u"Elevation"  # input("Digite sua busca:\n")
+    keyword = u"O Sol"  # input("Digite sua busca:\n")
     url = "http://cifraclub.com.br/"  # input("Digite a url de inicio:\n")
     depth = 3  # input("digite a profundidade\n")
     search(keyword, url, depth)
@@ -19,12 +19,14 @@ def search(keyword, url, depth):
     res = ""
     for n in nomes:
         lista_mais_acessadas += n
+    for i in lista_mais_acessadas:
+        if keyword in lista_mais_acessadas:
+            casos = casos + 1
     end = time.time()
     if casos < 1:
-        res = u"não está entre as mais tocadas."
+        res = u"não está entre as mais acessadas em:"
     else:
-        res = u"está entre as mais tocadas em:"
-
+        res = u"está entre as mais acessadas em:"
     print(u"\nSua busca retornou: " + "\"" + keyword + "\" " + res + "\n" + url)
     print(u"Tempo de execução:", end - start)
 
