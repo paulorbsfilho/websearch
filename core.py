dc2f3f7050d18cddfc445cf7f8a9412426bf8538
paulorbsfilho <p4ulinho@outlook.com>
2018-10-02 11:32:12 -0300

    Acesso a sublinks.

--- core.py
+++ core.py
@@ -3,32 +3,53 @@ import requests, time
 
 
 def main():
-    keyword = u"O Sol"  # input("Digite sua busca:\n")
-    url = "http://cifraclub.com.br/"  # input("Digite a url de inicio:\n")
-    depth = 3  # input("digite a profundidade\n")
+    keyword = u"Inscrição"  # input("Digite sua busca:\n")
+    url = "http://libra.ifpi.edu.br/"  # input("Digite a url de inicio:\n")
+    depth = 2  # input("digite a profundidade\n")
+    start = time.time()
     search(keyword, url, depth)
+    end = time.time()
+    print(u"Tempo de execução:", end - start)
 
 
 def search(keyword, url, depth):
-    start = time.time()
+
     response = requests.get(url)
     html = BeautifulSoup(response.text, 'html5lib')
-    nomes = html.find_all("strong", class_="top-txt_primary")
-    lista_mais_acessadas = []
-    casos = 0
-    res = ""
-    for n in nomes:
-        lista_mais_acessadas += n
-    for i in lista_mais_acessadas:
-        if keyword in lista_mais_acessadas:
-            casos = casos + 1
-    end = time.time()
-    if casos < 1:
-        res = u"não está entre as mais acessadas em:"
-    else:
-        res = u"está entre as mais acessadas em:"
-    print(u"\nSua busca retornou: " + "\"" + keyword + "\" " + res + "\n" + url)
-    print(u"Tempo de execução:", end - start)
+    links = html.find_all("a")
+    text = html.text
+    t = text.split()
+    s = 0
+    try:
+        s = t.index(keyword)
+        print(s)
+    except ValueError:
+        print(u"\nPalavra ou expressão nao encontrada dentro da página.\n")
+        print(url)
+    tam = len(t)
+    v = []
+    cp = 2
+    while cp != 0:
+        v.append(t[s - cp])
+        cp -= 1
+    c = 0
+    while c < 3:
+        if s + c < tam:
+            v.append(t[s + c])
+            c += 1
+        else:
+            break
+    res = " ".join(v)
+    if depth > 0:
+        for link in links:
+            try:
+                if link['href'].startswith('http'):
+                    search(keyword, link['href'], depth -1)
+            except KeyError:
+                print(u"Link inválido: " + str(link))
+
+    print(u"\nSua busca retornou: " + keyword + ". " + res + "\nLink: " + url)
+
 
 
 if __name__ == '__main__':
