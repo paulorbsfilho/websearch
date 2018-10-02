bbf4fc720112c536529e008a568404941be4b0f4
paulorbsfilho <p4ulinho@outlook.com>
2018-10-01 21:07:09 -0300

    Rollback.

--- core.py
+++ core.py
@@ -1,10 +1,11 @@
 from bs4 import BeautifulSoup
-import requests, re, time
+import requests, time
+
 
 def main():
-    keyword = "Federal"#input("Digite sua busca:\n")
-    url = "http://www.globoesporte.com/" #input("Digite a url de inicio:\n")
-    depth = 3#input("digite a profundidade\n")
+    keyword = u"Inscrição"  # input("Digite sua busca:\n")
+    url = "http://libra.ifpi.edu.br/"  # input("Digite a url de inicio:\n")
+    depth = 3  # input("digite a profundidade\n")
     search(keyword, url, depth)
 
 
@@ -13,15 +14,37 @@ def search(keyword, url, depth):
     response = requests.get(url)
     html = BeautifulSoup(response.text, 'html5lib')
     links = html.find_all("a")
-    for l in links:
-        print(l.get('href'))
-    searchInPage(keyword,html)
+    for link in links:
+        try:
+            print(link['href'])
+        except KeyError:
+            print(u"Não é um link")
+    text = html.text
+    t = text.split()
+    s = 0
+    try:
+        s = t.index(keyword)
+        print(type(s))
+    except ValueError:
+        print(u"\nPalavra ou expressão nao encontrada dentro da página.")
+    tam = len(t)
+    v = []
+    cp = 2
+    while cp != 0:
+        v.append(t[s - cp])
+        cp -= 1
+    c = 0
+    while c < 3:
+        if s + c < tam:
+            v.append(t[s + c])
+            c += 1
+        else:
+            break
     end = time.time()
+
+    res = " ".join(v)
+    print(u"\nSua busca retornou: " + keyword + ". "+ res + "\nLink: " + url)
     print(u"Tempo de execução:", end - start)
-
-
-def searchInPage(keyword,html):
-    pass
 
 
 if __name__ == '__main__':
